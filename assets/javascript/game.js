1dc5c3d7f08a6c77d221d0eea192185025a369c1
juan uribe <juanpablouribe@mac.com>
2019-10-02 18:41:36 -0400

    wrong letters prompt works but code doesnt tell user if they already guessed a letter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,6 +9,7 @@ var host = document.getElementById("message");
 var score = document.getElementById("liferemain");
 var wrongLetters = document.getElementById("lettersused");
 var guessesLeft = 10;
+lettersarray = [];
 score.textContent = guessesLeft;
 
 document.onkeyup = function (event) {
@@ -42,14 +43,9 @@ document.onkeyup = function (event) {
             guessesLeft = guessesLeft - 1
             score.textContent = guessesLeft;
             var nope = event.key;
-            lettersarray=[];
             lettersarray.push(nope);
-            for (i=0;i<lettersarray.length;i++){
-            wrongLetters.textContent=lettersarray[i];
-            }
             alert("Wrong!");
-            
-            
+            wrongLetters.textContent = lettersarray;
         }
         if (guessesLeft == 0) {
             document.write("<h1>game over</h1>");
@@ -57,6 +53,7 @@ document.onkeyup = function (event) {
         if (wordsList[wordIndex].first == "s" && wordsList[wordIndex].second == "m" && wordsList[wordIndex].third == "u" && wordsList[wordIndex].fourth == "r" && wordsList[wordIndex].fifth == "f") {
             wordIndex++;
             host.textContent = "Good Job!";
+            lettersarray = [];
             start.textContent = wordsList[wordIndex].first + " " + wordsList[wordIndex].second + " " + wordsList[wordIndex].third + " " + wordsList[wordIndex].fourth + " " + wordsList[wordIndex].fifth + " " + wordsList[wordIndex].sixth + " " + wordsList[wordIndex].seventh + " " + wordsList[wordIndex].eighth;
             document.onkeyup = function (event) {
                 function wordStatus() {
@@ -97,7 +94,10 @@ document.onkeyup = function (event) {
                 if (event.key !== "d" && event.key !== "i" && event.key !== "n" && event.key !== "o" && event.key !== "s" && event.key !== "a" && event.key !== "u" && event.key !== "r") {
                     guessesLeft = guessesLeft - 1
                     score.textContent = guessesLeft;
+                    var nope = event.key;
+                    lettersarray.push(nope);
                     alert("Wrong!");
+                    wrongLetters.textContent = lettersarray;
                 }
                 if (guessesLeft == 0) {
                     document.write("<h1>game over</h1>");
@@ -106,6 +106,7 @@ document.onkeyup = function (event) {
                 if (wordsList[wordIndex].first == "d" && wordsList[wordIndex].second == "i" && wordsList[wordIndex].third == "n" && wordsList[wordIndex].fourth == "o" && wordsList[wordIndex].fifth == "s" && wordsList[wordIndex].sixth == "a" && wordsList[wordIndex].seventh == "u" && wordsList[wordIndex].eighth == "r") {
                     wordIndex++;
                     host.textContent = "Yay, two words down..!!";
+                    lettersarray = [];
                     start.textContent = wordsList[wordIndex].first + " " + wordsList[wordIndex].second + " " + wordsList[wordIndex].third + " " + wordsList[wordIndex].fourth + " " + wordsList[wordIndex].fifth + " " + wordsList[wordIndex].sixth;
                     document.onkeyup = function (event) {
                         function wordStatus() {
@@ -129,13 +130,17 @@ document.onkeyup = function (event) {
                             wordsList[wordIndex].fifth = "y";
                             wordStatus();
                         }
+                        if (guessesLeft == 0) {
+                            document.write("<h1>game over</h1>");
+                        }
                         if (event.key !== "p" && event.key !== "o" && event.key !== "e" && event.key !== "y") {
                             guessesLeft = guessesLeft - 1
                             score.textContent = guessesLeft;
+                            var nope = event.key;
+                            lettersarray.push(nope);
                             alert("Wrong!");
-                            if (guessesLeft == 0) {
-                                document.write("<h1>game over</h1>");
-                            }
+                            wrongLetters.textContent = lettersarray;
+
                         }
                         if (wordsList[wordIndex].first == "p" && wordsList[wordIndex].second == "o" && wordsList[wordIndex].third == "p" && wordsList[wordIndex].fourth == "e" && wordsList[wordIndex].fifth == "y" && wordsList[wordIndex].sixth == "e") {
                             // alert("Wow!! you did it");
